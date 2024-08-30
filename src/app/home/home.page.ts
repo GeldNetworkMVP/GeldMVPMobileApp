8a9aa4c6097048596acdb2c42f7fd1dd11487f23
Nethsara Sandeepa Elvitigala <nethsarasandeepaelvitigala@gmail.com>
2024-08-30 23:23:55 +0530

    chore: setup primeng

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IonToolbar, IonHeader, IonContent, IonTitle } from "@ionic/angular/standalone";
+import {ButtonModule} from 'primeng/button'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonTitle, IonContent, IonHeader, IonToolbar],
+  imports: [IonTitle, IonContent, IonHeader, IonToolbar, ButtonModule],
   standalone: true
 })
 export class HomePage {}
