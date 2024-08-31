30e1af2f99215096caaf4655b399a0104876576f
Nethsara Sandeepa Elvitigala <nethsarasandeepaelvitigala@gmail.com>
2024-08-31 18:33:34 +0530

    feat: add toggle sidebar button and basic sidebar

--- src/app/features/home/home.page.ts
+++ src/app/features/home/home.page.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { IonToolbar, IonHeader, IonContent, IonTitle } from "@ionic/angular/standalone";
-import {ButtonModule} from 'primeng/button'
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonToolbar, IonHeader, IonContent, IonTitle, IonMenu, IonMenuToggle, IonButton } from "@ionic/angular/standalone";
+import { ButtonModule } from 'primeng/button'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonTitle, IonContent, IonHeader, IonToolbar, ButtonModule],
+  imports: [IonTitle, IonContent, IonHeader, IonButton, IonToolbar, IonMenu, IonMenuToggle, ButtonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   standalone: true
 })
-export class HomePage {}
+export class HomePage { }
