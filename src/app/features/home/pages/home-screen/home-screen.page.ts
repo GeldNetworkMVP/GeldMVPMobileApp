58531e8267a7390dbea77a5fa0d785c78ac9f4c3
Nethsara Sandeepa Elvitigala <nethrenial@HaulMatics-MacBook-Air.local>
2024-09-03 11:30:12 +0530

    feat: add all links and buttons to sidebar

--- src/app/features/home/pages/home-screen/home-screen.page.ts
+++ src/app/features/home/pages/home-screen/home-screen.page.ts
@@ -1,13 +1,71 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { IonToolbar, IonHeader, IonContent, IonTitle, IonMenu, IonMenuToggle, IonButton } from "@ionic/angular/standalone";
-import { ButtonModule } from 'primeng/button'
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import {
+  IonToolbar,
+  IonHeader,
+  IonContent,
+  IonTitle,
+  IonMenu,
+  IonMenuToggle,
+  IonButton,
+} from '@ionic/angular/standalone';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+
+type SideNavItem = {
+  label: string;
+  icon?: string;
+  route?: string;
+  action?: () => void;
+};
 
 @Component({
   selector: 'app-home-screen',
   templateUrl: './home-screen.page.html',
   styleUrls: ['./home-screen.page.scss'],
-  imports: [IonTitle, IonContent, IonHeader, IonButton, IonToolbar, IonMenu, IonMenuToggle, ButtonModule],
+  imports: [
+    RouterLink,
+    IonTitle,
+    IonContent,
+    IonHeader,
+    IonButton,
+    IonToolbar,
+    IonMenu,
+    IonMenuToggle,
+    ButtonModule,
+    DividerModule,
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  standalone: true
+  standalone: true,
 })
-export class HomeScreenPage { }
+export class HomeScreenPage {
+  router = inject(Router);
+
+  goTo(route: string) {
+    this.router.navigate([route]);
+  }
+
+  logout() {
+    console.log('logout');
+  }
+
+  sideNavItems: SideNavItem[] = [
+    {
+      label: 'Edit User Profile',
+      route: '/edit-user-profile',
+    },
+    {
+      label: 'New Data Template',
+      route: '/new-data-template',
+    },
+    {
+      label: 'View Data Templates',
+      route: '/view-data-templates',
+    },
+    {
+      label: 'Logout',
+      action: this.logout,
+      icon: 'lucide:log-out',
+    },
+  ];
+}
