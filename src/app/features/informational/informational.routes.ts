173e0c4e8fa7b09067ccf8a10d3c22c47d27cd9f
Nethsara Elvitigala <nethsarasandeepaelvitigala@gmail.com>
2024-09-05 00:21:15 +0530

    feat: setup android project

--- src/app/features/informational/informational.routes.ts
+++ src/app/features/informational/informational.routes.ts
@@ -1,5 +1,12 @@
-import { Routes } from "@angular/router";
+import { Routes } from '@angular/router';
 
 export const informationalRoutes: Routes = [
-    { path: 'welcome', loadComponent: () => import('./pages/welcome-screen/welcome-screen.page').then(m => m.WelcomeScreenPage) },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  {
+    path: 'welcome',
+    loadComponent: () =>
+      import('./pages/welcome-screen/welcome-screen.page').then(
+        (m) => m.WelcomeScreenPage
+      ),
+  },
 ];
